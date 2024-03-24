b8159e7619f8a2d7aa11adb9ddaa21447ded1374
sgfrdgrln <gazel0123@gmail.com>
2024-03-24 18:41:52 +0800

    edited Welcome.js

--- frontend/src/features/auth/Welcome.js
+++ frontend/src/features/auth/Welcome.js
@@ -16,9 +16,9 @@ const Welcome = () => {
 
             <p><Link to="/dash/users">View User Settings</Link></p>
 
-            <p><Link to="/dash/notes">Add New techNotes</Link></p>
+            <p><Link to="/dash/notes/new">Add New techNotes</Link></p>
 
-            <p><Link to="/dash/users">Add New User</Link></p>
+            <p><Link to="/dash/users/new">Add New User</Link></p>
 
         </section>
     )
