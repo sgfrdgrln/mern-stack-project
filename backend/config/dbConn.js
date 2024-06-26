19da39bd93308af5c8422803f97dabc624d52bc1
sieg <99021879+sgfrdgrln@users.noreply.github.com>
2024-06-27 01:47:21 +0800

    Update dbConn.js

--- backend/config/dbConn.js
+++ backend/config/dbConn.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(LOCAL_DATABASE_URI)
+        await mongoose.connect(process.env.LOCAL_DATABASE_URI)
     } catch (err) {
         console.log(err)
     }
